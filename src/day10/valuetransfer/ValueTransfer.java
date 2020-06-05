d94930de2c7d933f50f755ab4b6aad486888df96
yang1583148668 <1583148668@qq.com>
2020-06-05 17:19:56 +0800

    修改值传递编码格式

--- src/day10/valuetransfer/ValueTransfer.java
+++ src/day10/valuetransfer/ValueTransfer.java
@@ -1,6 +1,5 @@
 package day10.valuetransfer;
 
-import javafx.collections.ListChangeListener.Change;
 
 /*
  *   方法参数传递的原则也是值传递
@@ -18,12 +17,12 @@ public class ValueTransfer {
 		//基本数据类型传参只传i1的值，在其他方法中会新建变量i1 用来存储这个值，对其他方法中的i1做修改并不改变
 		//main方法中i1的值
 		System.out.println("基本数据类型测试 改变后"+i1);
-		
+
 		ValueHelp vh1 = new ValueHelp();
 		System.out.println("引用数据类型测试 改变前"+vh1.i);
 		changeYinYong(vh1);
 		System.out.println("引用数据类型测试 改变后"+vh1.i);
-		
+
 		String s1 = "abc";
 		String s2 = new String("edf");
 		System.out.println("字符串数据类型测试 1改变前"+s1);
@@ -33,19 +32,19 @@ public class ValueTransfer {
 		System.out.println("字符串数据类型测试 1改变前"+s1);
 		System.out.println("字符串数据类型测试 2改变前"+s2);
 	}
-	
+
 	public static void changeBasic(int i1) {
 		i1=2;
 	}
-	
+
 	public static void changeYinYong(ValueHelp vh) {
 		vh.i  =20;
 	}
-	
+
 	public static void changeString1(String s) {
 		s = "hij";
 	}
-	
+
 	public static void changeString2(String s) {
 		s = new String("opq");
 	}
